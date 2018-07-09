956bbd62968e0311b0d15206cd44524a4e375b56
GatoCreador887 <36546666+GatoCreador887@users.noreply.github.com>
2018-07-09 15:32:33 -0400

    Add files via upload

---
+++ ModdingUtils.js
@@ -0,0 +1,19 @@
+function randomIntInRange(min, max) {
+  return Math.floor(Math.random()  (max - min + 1) + min);
+}
+
+function randomDecimalInRange(min, max) {
+  return Math.random()  (max - min) + min;
+}
+
+function randomVelocity(speed) {
+  var phi = 2  Math.PI  Math.random();
+  var vx = speed  Math.cos(phi);
+  var vy = speed  Math.sin(phi);
+  
+  return {vxvx,vyvy};
+}
+
+function chooseRandom(choices) {
+  return choices[Math.floor(Math.random()  choices.length)];
+}
